a582a304e9555342e189a57c6d5896778c73bf63
Yash Alapuria <67858039+Yashraj2502@users.noreply.github.com>
2023-02-04 17:57:47 +0530

    Add files via upload

---
+++ hackerRank/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scanner = new Scanner(System.in);
+        int i1 = scanner.nextInt();
+        double d1 = scanner.nextDouble();
+
+        Scanner input = new Scanner(System.in);
+        String s1 = input.nextLine();
+
+        String s2 = s.concat(s1);
+
+        System.out.println(i + i1);
+        System.out.println(d + d1);
+        System.out.println(s2);
+    }
+}
